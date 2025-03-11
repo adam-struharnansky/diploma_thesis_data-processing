9d3193525da7fafd83f1038349f3ec5d94911255
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-03-11 14:33:16 +0100

    files for star aligner

--- scripts/install_star.sh
+++ scripts/install_star.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Create a new conda environment (optional)
-conda create -n star_env python=3.8 -y
+# Create a new conda environment with Python 3.8
+conda create -n star_env -y
 
 # Activate the new environment
-source activate star_aligner_env
+source activate star_env
 
-# Install STAR using the bioconda channel
-conda install -c bioconda star -y
+# Install STAR and samtools
+conda install -c bioconda -c conda-forge star=2.7.10b samtools=1.9 -y
