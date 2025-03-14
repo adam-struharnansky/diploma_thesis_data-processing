d6466dc260c6d828f57e3771d3be9613e5e773ff
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-03-14 17:32:27 +0100

    featureCounts for star

---
+++ counts/star_featureCounts_all_seqcB/run_star_featureCounts_all_seqcB.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Activate conda environment
+source miniconda3/etc/profile.d/conda.sh
+conda activate feature_counts_env
+
+# Set parameters
+INPUT_DIR="genetic_data/alignments/star_seqcB_all"
+GTF_FILE="genetic_data/annotations/gencode.v19.annotation.gtf.gz"
+OUTPUT_DIR="genetic_data/counts/star_featureCounts_all_seqcB"
+THREADS=4
+
+# Ensure output directory exists
+mkdir -p "$OUTPUT_DIR"
+
+# Loop through BAM files
+for BAM_FILE in "$INPUT_DIR"/*_sorted_by_name.bam; do
+    # Extract the sample name
+    SAMPLE_NAME=$(basename "$BAM_FILE" _sorted_by_name.bam)
+
+    echo "Processing $SAMPLE_NAME..."
+
+    # Run featureCounts with different multimapped read options
+    featureCounts -p -T "$THREADS" -a "$GTF_FILE" -o "$OUTPUT_DIR/${SAMPLE_NAME}_ignore.txt" "$BAM_FILE"
+
+    featureCounts -p -M -T "$THREADS" -a "$GTF_FILE" -o "$OUTPUT_DIR/${SAMPLE_NAME}_best.txt" "$BAM_FILE"
+
+    featureCounts -p -M -O -T "$THREADS" -a "$GTF_FILE" -o "$OUTPUT_DIR/${SAMPLE_NAME}_all.txt" "$BAM_FILE"
+
+    featureCounts -p -M --fraction -O -T "$THREADS" -a "$GTF_FILE" -o "$OUTPUT_DIR/${SAMPLE_NAME}_fraction.txt" "$BAM_FILE"
+
+    echo "Finished processing $SAMPLE_NAME"
+done
+
+echo "All files processed!"
