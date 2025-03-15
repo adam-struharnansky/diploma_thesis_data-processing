64bc1b8594a1d255ba069d27aeaf6d3c1800900b
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-03-15 11:20:54 +0100

    salmon + kallisto for beers

---
+++ indexes/salmon_index_mus_musculus/create_salmon_index_mus_musculus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Activate conda environment
+source miniconda3/etc/profile.d/conda.sh
+conda activate salmon_env
+
+# Set parameters
+TRANSCRIPTOME_FILE="genetic_data/transcriptomes/Mus_musculus.GRCm38.cdna.fa.gz"
+OUTPUT_DIR="genetic_data/indexes/salmon_index_mus_musculus"
+
+# Create output directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+
+# Run Salmon index
+salmon index -t "$TRANSCRIPTOME_FILE" -i "$OUTPUT_DIR" --gencode
