f6861e3dd43bf814ff1455d4a5281adaa8914727
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-04-18 18:24:21 +0200

    rat counts

---
+++ counts/star_HTSeq_rat/run_star_HTSeq_rat.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Activate conda environment
+source miniconda3/etc/profile.d/conda.sh
+conda activate htseq_env
+
+# Set parameters
+INPUT_DIR="genetic_data/alignments/star_rat_all"
+GTF_FILE="genetic_data/annotations/Rattus_norvegicus.Rnor_5.0.77.gtf.gz"
+OUTPUT_DIR="genetic_data/counts/star_HTSeq_rat"
+
+# Create output directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+
+# Loop through name-sorted BAM files
+for BAM_FILE in "$INPUT_DIR"/*_sorted_by_name.bam; do
+    SAMPLE_NAME=$(basename "$BAM_FILE" _sorted_by_name.bam)
+
+    echo "Processing $SAMPLE_NAME..."
+
+    # Determine HTSeq strand option
+    if [[ "$SAMPLE_NAME" == *_mirna_* ]]; then
+        STRAND_OPTION="yes"
+    else
+        STRAND_OPTION="no"
+    fi
+
+    # Run HTSeq-count with different multimapped read options
+    htseq-count -f bam -r name -s "$STRAND_OPTION" -t exon -i gene_id \
+        "$BAM_FILE" "$GTF_FILE" > "$OUTPUT_DIR/${SAMPLE_NAME}_ignore.txt"
+
+    htseq-count -f bam -r name -s "$STRAND_OPTION" -t exon -i gene_id --nonunique all \
+        "$BAM_FILE" "$GTF_FILE" > "$OUTPUT_DIR/${SAMPLE_NAME}_all.txt"
+
+    htseq-count -f bam -r name -s "$STRAND_OPTION" -t exon -i gene_id --nonunique fraction \
+        "$BAM_FILE" "$GTF_FILE" > "$OUTPUT_DIR/${SAMPLE_NAME}_fraction.txt"
+
+    echo "Finished processing $SAMPLE_NAME"
+done
+
+echo "All files processed!"
