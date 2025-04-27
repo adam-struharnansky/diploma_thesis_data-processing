bbccc82dba424befcfe99f7388bd30260931cd87
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-04-27 08:04:00 +0200

    mirna for star htseq rat

--- counts/bowtie2_HTSeq_rat/run_bowtie2_HTSeq_rat.sh
+++ counts/bowtie2_HTSeq_rat/run_bowtie2_HTSeq_rat.sh
@@ -6,7 +6,8 @@ conda activate htseq_env
 
 # Set parameters
 INPUT_DIR="genetic_data/alignments/bowtie2_rat_all" 
-GTF_FILE="genetic_data/annotations/Rattus_norvegicus.Rnor_5.0.77.gtf.gz"
+GTF_GENE="genetic_data/annotations/Rattus_norvegicus.Rnor_5.0.77.gtf.gz"
+GTF_MIRNA="genetic_data/annotations/miRNA_Rattus_norvegicus.Rnor_5.0.77.gtf.gz"
 OUTPUT_DIR="genetic_data/counts/bowtie2_HTSeq_rat"
 
 # Create output directory if it doesn't exist
@@ -21,8 +22,10 @@ for BAM_FILE in "$INPUT_DIR"/*_sorted_by_name.bam; do
     # Determine HTSeq strand option
     if [[ "$SAMPLE_NAME" == *_mirna_* ]]; then
         STRAND_OPTION="yes"
+        GTF_FILE="$GTF_MIRNA"
     else
         STRAND_OPTION="no"
+        GTF_FILE="$GTF_GENE"
     fi
 
     # Run HTSeq-count with different multimapped read handling
