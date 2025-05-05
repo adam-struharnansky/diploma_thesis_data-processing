fe7e55636eb3c2ee4be5a8a3ec96574508f3593b
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-05-05 14:49:15 +0200

    fix

--- outputs/transform_counts.py
+++ outputs/transform_counts.py
@@ -536,7 +536,7 @@ def process_rat_directory(directory_path, tool_type, gene_lengths_df=None, main_
             else:
                 df = pd.merge(main_table, df, left_on='target_ensemble_id', right_on='gene_id', how='left')
                 all_dataframes.append(df)
-    if all_dataframes:
+    if all_dataframes and all_mirna_dataframes:
         result_df = all_dataframes[0]
         result_mirna_df = all_mirna_dataframes[0]
         for df in all_dataframes[1:]:
