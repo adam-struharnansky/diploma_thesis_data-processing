a3a445635f8bc8b2b9fad8ad2148d6890c41f1e1
Adam Struharnansky <adam.struharnansky@gmail.com>
2025-05-09 12:46:54 +0200

    debug

--- outputs/transform_counts.py
+++ outputs/transform_counts.py
@@ -147,6 +147,8 @@ def process_htseq(filepath, gene_lengths_df):
     df = df[~df["gene_id"].str.startswith("__")]
     df['gene_id'] = df['gene_id'].apply(strip_version)
     df['counts'] = pd.to_numeric(df['counts'], errors='coerce')
+    print(df.head())
+    print(gene_lengths_df.head())
     merged = pd.merge(df, gene_lengths_df, on='gene_id', how='inner') # Merging with lengths
     merged["TPM"] = (merged["counts"] / merged["gene_length"]) / (merged["counts"] / merged["gene_length"]).sum() * 1e6 # TPM calculation
     print(merged[["gene_id", "TPM"]].head())
